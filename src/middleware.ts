adf1a5c73ef8e235251bb763dc863009ccda7f9e
Evandro <dam450@duck.com>
2024-10-10 12:04:37 -0300

    ♻️  refactor/FSK-7: update auth logic in middleware for edge function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,9 @@
-export { auth as middleware } from '@/lib/auth';
+import NextAuth from 'next-auth';
+
+import { authConfig } from '@/lib/auth/auth.config';
+
+export const { auth: middleware } = NextAuth(authConfig);
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
+};
