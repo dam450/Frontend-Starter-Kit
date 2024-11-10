fb5919825da50c43a7b8160d3e4c6443a379ea0d
Evandro <dam450@duck.com>
2024-11-10 20:01:05 -0300

    👔 chore/FSK-14: redirects pages admin and login if user has or not a session

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,13 @@
+import { redirect } from 'next/navigation';
+
 import UserAvatar from '@/components/user-avatar';
+import { auth } from '@/lib/auth';
+
+export default async function AdminPage() {
+  const session = await auth();
+
+  if (!session) return redirect('/login');
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-3xl">Admin</h1>
